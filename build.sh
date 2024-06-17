a3967a2a7eb0df837a5bb8a3117f0a3783967071
François <francois.mockers@vleue.com>
2024-06-17 08:15:38 +0200

    don't need to change path on custom domain

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ npm run build
 cd ..
 cp -r graph-ui/dist/index.html public/training/index.html
 cp -r graph-ui/dist/_next public/_next
-# just for current way of publish on github page, should not run locally
-# it works without a condition because this command fails on macos
-sed -i 's/\/_next/\/build-your-own\/_next/g' public/training/index.html
